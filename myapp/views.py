49a3fd2a5522b35015edfa271e1380756b123547
M S Shiraj <msshiraj1999@gmail.com>
2022-08-06 18:02:31 +0530

    Few conditions added

--- myapp/views.py
+++ myapp/views.py
@@ -14,18 +14,17 @@ def home(request):
     data= response['data']
     index=randint(0,len(data)-1)
 
-    obj= data[index]
+    obj= data[index]        
     return render(request,'home.html',{'response':obj})
 
 def check(request):
-    print("My request",request)
-    country1 = request.GET['country']
-    print("Country name", country1)
-    capital1 = request.GET['capital']
+    # print("My request",request)         United Nations
+    country1 = request.GET['country']   # United
+    # print("Country name", country1)
+    capital1 = request.GET['capital']     
     actualcapital = request.GET['actualcapital']
-    print("Country name", actualcapital)
-    print("Checked")
+    # print("Country name", actualcapital)
     if capital1.lower()==actualcapital.lower():
         return render(request,'result.html',{'country1':country1, 'resCorrect':"Your Answer is Correct", 'resInCorrect':""})
     else:
-        return render(request,'result.html',{'country1':country1, 'resCorrect':"", 'resInCorrect':actualcapital})
+        return render(request,'result.html',{'country1':country1, 'resCorrect':"", 'resInCorrect':"The correct answer is:"+actualcapital})
